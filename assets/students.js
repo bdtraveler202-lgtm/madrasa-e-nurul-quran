a9b9fc0331215775504be92ad0b75d73cd7ef11d
bdtraveler202-lgtm <bdtraveler202@gmail.com>
2026-07-17 17:43:23 +0600

    Update students.js

--- assets/students.js
+++ assets/students.js
@@ -1,1 +1,45 @@
+async function loadStudents() {
 
+    const { data, error } = await window.supabaseClient
+        .from("students")
+        .select("*")
+        .order("id", { ascending: true });
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    const table = document.getElementById("studentTable");
+
+    table.innerHTML = "";
+
+    data.forEach(student => {
+
+        table.innerHTML += `
+
+<tr>
+
+<td>${student.id}</td>
+
+<td>${student.full_name}</td>
+
+<td>${student.father_name}</td>
+
+<td>${student.mother_name}</td>
+
+<td>${student.mobile}</td>
+
+<td>${student.class}</td>
+
+<td>${student.address}</td>
+
+</tr>
+
+`;
+
+    });
+
+}
+
+loadStudents();
