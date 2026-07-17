dc2e9670da6e755dd0c3d9ba7dc636a45e625940
bdtraveler202-lgtm <bdtraveler202@gmail.com>
2026-07-17 17:08:50 +0600

    Update supabase.js

--- assets/supabase.js
+++ assets/supabase.js
@@ -1,15 +1,9 @@
 const SUPABASE_URL = "https://fzrwcibouvazfreushvi.supabase.co";
 
-const SUPABASE_ANON_KEY = "sb_publishable_HC_zCo3NhP00Sp2a61UNSA_0Btr3II2";
+const SUPABASE_KEY =
+"sb_publishable_HC_zCo3NhP00Sp2a61UNSA_0Btr3II2";
 
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_KEY = "YOUR_PUBLISHABLE_KEY";
-
-const supabase = window.supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
