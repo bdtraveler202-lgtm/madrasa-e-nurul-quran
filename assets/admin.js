4f10716903c3780950d73ab0bd410ea3406899e0
bdtraveler202-lgtm <bdtraveler202@gmail.com>
2026-07-17 23:39:34 +0600

    Update admin.js

--- assets/admin.js
+++ assets/admin.js
@@ -1,1 +1,34 @@
+// ===============================
+// Admin Dashboard
+// ===============================
 
+async function loadDashboard() {
+
+    const { data, error } = await window.supabaseClient
+        .from("students")
+        .select("*");
+
+    if (error) {
+        console.error(error);
+        alert("ডেটা লোড করা যায়নি!");
+        return;
+    }
+
+    // Total Students
+    document.getElementById("totalStudents").innerText = data.length;
+
+    // Nurani
+    const nurani = data.filter(student => student.class === "নূরানী").length;
+    document.getElementById("nurani").innerText = nurani;
+
+    // Nazera
+    const nazera = data.filter(student => student.class === "নাজেরা").length;
+    document.getElementById("nazera").innerText = nazera;
+
+    // Hifz
+    const hifz = data.filter(student => student.class === "হিফজ").length;
+    document.getElementById("hifz").innerText = hifz;
+
+}
+
+loadDashboard();
