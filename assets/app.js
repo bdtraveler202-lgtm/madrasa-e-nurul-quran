cdb0c76c21a2e07b772bbce3f05df2a74511101c
bdtraveler202-lgtm <bdtraveler202@gmail.com>
2026-07-19 00:26:19 +0600

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -65,3 +65,22 @@ behavior:"smooth"
 });
 
 };
+// ==========================
+// Navbar Shadow
+// ==========================
+
+const navbar=document.querySelector(".navbar");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>50){
+
+navbar.classList.add("scrolled");
+
+}else{
+
+navbar.classList.remove("scrolled");
+
+}
+
+});
