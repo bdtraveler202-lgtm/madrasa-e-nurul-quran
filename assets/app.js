d58a62df48e44310235cc0cf5150ef78ec5dfa9a
bdtraveler202-lgtm <bdtraveler202@gmail.com>
2026-07-19 00:25:17 +0600

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -34,3 +34,34 @@ if (admissionForm) {
   });
 
 }
+// ==========================
+// Back To Top
+// ==========================
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>300){
+
+topBtn.style.display="block";
+
+}else{
+
+topBtn.style.display="none";
+
+}
+
+});
+
+topBtn.onclick=()=>{
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+};
