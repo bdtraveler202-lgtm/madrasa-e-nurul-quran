3903b7640a2e2feda705b98c6658aee3b9dc7145
bdtraveler202-lgtm <bdtraveler202@gmail.com>
2026-07-27 22:14:15 +0600

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,1 +1,64 @@
+// ======================================
+// LOGIN
+// ======================================
 
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", async function(e){
+
+e.preventDefault();
+
+const email =
+document.getElementById("email").value.trim();
+
+const password =
+document.getElementById("password").value;
+
+const btn =
+document.querySelector(".btn-login");
+
+btn.disabled = true;
+
+btn.innerHTML = "Signing In...";
+
+const { error } =
+await window.supabaseClient.auth.signInWithPassword({
+
+email,
+
+password
+
+});
+
+btn.disabled = false;
+
+btn.innerHTML = "Login";
+
+if(error){
+
+alert(error.message);
+
+return;
+
+}
+
+window.location.href = "admin.html";
+
+});
+
+// ======================================
+// AUTO REDIRECT
+// ======================================
+
+(async()=>{
+
+const { data } =
+await window.supabaseClient.auth.getSession();
+
+if(data.session){
+
+window.location.href="admin.html";
+
+}
+
+})();
