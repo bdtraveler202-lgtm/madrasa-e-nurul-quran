e49a09cfddb9bfb4780ce65f2461e5aa31ec8a21
bdtraveler202-lgtm <bdtraveler202@gmail.com>
2026-07-27 22:00:17 +0600

    Delete assets directory

--- assets/supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://fzrwcibouvazfreushvi.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_HC_zCo3NhP00Sp2a61UNSA_0Btr3II2";
-
-window.supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
