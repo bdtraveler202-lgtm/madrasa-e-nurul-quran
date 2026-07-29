7ece068555c30b4b14ff7dcde314cf03471517d9
bdtraveler202-lgtm <bdtraveler202@gmail.com>
2026-07-30 02:35:45 +0600

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,1 +1,11 @@
+/* ===========================================
+   SUPABASE CONFIG
+=========================================== */
 
+const SUPABASE_URL = "sbuwokmghcqjpjxcuywa";
+const SUPABASE_ANON_KEY = "sb_publishable_zWUKFexK6Mv6FO6dTfeDBg_ExYrCkd_";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
