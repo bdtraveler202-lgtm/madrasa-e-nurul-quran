f678c9f3c51138a599ed8b39196d72987485ec27
bdtraveler202-lgtm <bdtraveler202@gmail.com>
2026-07-31 00:46:09 +0600

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -2,8 +2,8 @@
    SUPABASE CONFIG
 =========================================== */
 
-const SUPABASE_URL = "sbuwokmghcqjpjxcuywa";
-const SUPABASE_ANON_KEY = "sb_publishable_zWUKFexK6Mv6FO6dTfeDBg_ExYrCkd_";
+const SUPABASE_URL = "https://sbuwokmghcqjpjxcuywa.supabase.co";
+const SUPABASE_ANON_KEY = "তোমার_Anon_Public_Key";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
