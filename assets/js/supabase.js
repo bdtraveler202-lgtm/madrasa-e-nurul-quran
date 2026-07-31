d705bd36005205bab3102962e6bdcad5e31d09ea
bdtraveler202-lgtm <bdtraveler202@gmail.com>
2026-07-31 09:17:47 +0600

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,12 +1,9 @@
-/* ===========================================
-   SUPABASE CONFIG
-=========================================== */
-
 const SUPABASE_URL = "https://sbuwokmghcqjpjxcuywa.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_zWUKFexK6Mv6FO6dTfeDBg_ExYrCkd_";
 
-// Global client
-window.supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
+const { createClient } = window.supabase;
+
+window.supabaseClient = createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
